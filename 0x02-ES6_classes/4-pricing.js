ba5e1ed5c5775d4ca0f082b24118b0f4dcd82620
ewurapoms <mzpomaa35@gmail.com>
2024-02-08 14:45:08 +0000

    Import task 3 and implement a class named Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(quantity) {
+    this._amount = quantity;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(quantity) {
+    this._currency = quantity;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
