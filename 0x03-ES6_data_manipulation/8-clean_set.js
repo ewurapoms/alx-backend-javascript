ac4e74c1f59db183424c6112da3cf1a3d8afbb1a
ewurapoms <mzpomaa35@gmail.com>
2024-02-13 09:27:29 +0000

    debugging task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
-export default function cleanSet(set, startString) {
-  if (startString === undefined || startString.length === 0) {
+export default function cleanSet(set, initiate) {
+  if (initiate === undefined || initiate.length === 0) {
     return '';
   }
   return [...set]
-    .filter((element) => (key !== undefined ? key.startsWith(startString) : ''))
-    .map((key) => (key !== undefined ? key.slice(startString.length) : ''))
+    .filter((key) => key !== undefined ? key.startsWith(initiate) : '')
+    .map((key) => key !== undefined ? key.slice(initiate.length) : '')
     .join('-');
 }
