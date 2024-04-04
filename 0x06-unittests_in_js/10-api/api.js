5daaf16e8b05302e99054b1242351f18830e1a8c
ewurapoms <mzpomaa35@gmail.com>
2024-04-04 14:52:06 -0700

    correcting file contents

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,16 +3,31 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
-app.get('/', (request, response) => {
-  response.send('Welcome to the payment system');
-});
+app.use(express.json())
 
-app.get('/cart/:id([0-9]+)', (request, response) => {
-    response.send(`Payment methods for cart ${request.params.id}`);
-});
+app.get('/', (req, res) => {
+    res.send('Welcome to the payment system');
+})
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+    const id = req.params.id;
+    res.send(`Payment methods for cart ${id}`)
+})
+
+app.get('/available_payments', (req, res) => {
+    res.json({ payment_methods: { credit_cards: true, paypal: false } })
+})
+
+app.post('/login', (req, res) => {
+    let username = '';
+    if (req.body) {
+        username = req.body.userName;
+    }
+    res.send(`Welcome ${username}`);
+})
 
 app.listen(port, () => {
-    console.log(`API available on localhost port ${port}`);
-});
+    console.log(`API available on localhost port ${port}`)
+})
 
 module.exports = app;
