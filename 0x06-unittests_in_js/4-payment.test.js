0b631b8c45e54a3f007797e8574b6027c1d6f250
ewurapoms <mzpomaa35@gmail.com>
2024-04-04 14:44:00 -0700

    fixing requirement error

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -3,14 +3,17 @@ const { it, describe } = require('mocha');
 const sinon = require('sinon');
 
 const Utils = require('./utils.js');
-const sendPaymentRequestToApi = require('./3-payment.js');
+const sendPaymentRequestToApi = require('./4-payment.js');
 
 describe('', () => {
-  it('checking if numbers round with spies', () => {
-    const checkSoy = sinon.spy(Utils, 'calculateNumber');
-    sendPaymentRequestToApi(1, 3);
+  const checkSoy = sinon.spy(console, 'log');
+  it('checking if numbers round with spies and stubs', () => {
+    sendPaymentRequestToApi(100, 20);
+    const stubBoy = sinon.stub(Utils, 'calculateNumber');
+    stubBoy.withArgs('SUM', 100, 20).returns(120);
     expect(checkSoy.calledOnce).to.be.true;
-    expect(checkSoy.calledWith('SUM', 1, 3)).to.be.true;
+    expect(console.log('The total is: 120')).to.be.all;
     checkSoy.restore();
+    stubBoy.restore();
   });
 });
